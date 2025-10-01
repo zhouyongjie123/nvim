6551a0526b993ab011f4eca162483694b6f66b98
冰咖啡 <z1876849563@163.com>
2025-10-01 16:35:13 +0800

    save

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.maplocalleader = " "
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
- 
+
 local map = vim.api.nvim_set_keymap
 local set = vim.keymap.set
 -- 复用 opt 参数
-local opt = {noremap = true, silent = true }
- 
+local opt = { noremap = true, silent = true }
+
 -- 取消 s 默认功能
 map("n", "s", "", opt)
 -- windows 分屏快捷键
@@ -23,7 +24,7 @@ map("n", "<A-h>", "<C-w>h", opt)
 map("n", "<A-j>", "<C-w>j", opt)
 map("n", "<A-k>", "<C-w>k", opt)
 map("n", "<A-l>", "<C-w>l", opt)
- 
+
 -- 左右比例控制
 map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
 map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
@@ -36,7 +37,7 @@ map("n", "<C-Down>", ":resize +2<CR>", opt)
 map("n", "<C-Up>", ":resize -2<CR>", opt)
 -- 等比例
 map("n", "s=", "<C-w>=", opt)
- 
+
 -- Terminal相关
 map("n", "<leader>t", ":sp | terminal<CR>", opt)
 map("n", "<leader>vt", ":vsp | terminal<CR>", opt)
@@ -45,33 +46,33 @@ map("t", "<A-h>", [[ <C-\><C-N><C-w>h ]], opt)
 map("t", "<A-j>", [[ <C-\><C-N><C-w>j ]], opt)
 map("t", "<A-k>", [[ <C-\><C-N><C-w>k ]], opt)
 map("t", "<A-l>", [[ <C-\><C-N><C-w>l ]], opt)
- 
+
 -- visual模式下缩进代码
 map("v", "<", "<gv", opt)
 map("v", ">", ">gv", opt)
 -- 上下移动选中文本
 map("v", "J", ":move '>+1<CR>gv-gv", opt)
 map("v", "K", ":move '<-2<CR>gv-gv", opt)
- 
+
 -- 上下滚动浏览
 map("n", "J", "6j", opt)
 map("n", "K", "6k", opt)
- 
+
 -- 在visual 模式里粘贴不要复制
 map("v", "p", '"_dP', opt)
- 
+
 -- insert 模式下，跳到行首行尾
 map("i", "<C-h>", "<ESC>i", opt)
 map("i", "<C-l>", "<ESC>la", opt)
 map("i", "jj", "<ESC>", opt)
- 
+
 -- nvim-tree
 map("n", "<leader>e", ":NvimTreeToggle<CR>", opt)
 local pluginKeys = {}
 -- 列表快捷键
 pluginKeys.nvimTreeList = {
   -- 打开文件或文件夹
-  { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit" },
+  { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
   -- 分屏打开文件
   { key = "v", action = "vsplit" },
   { key = "h", action = "split" },
@@ -99,10 +100,10 @@ map("n", "<leader>bl", ":BufferLineCloseRight<CR>", opt)
 map("n", "<leader>bh", ":BufferLineCloseLeft<CR>", opt)
 map("n", "<leader>bc", ":BufferLinePickClose<CR>", opt)
 
-local builtin = require('telescope.builtin')
-set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+-- local builtin = require('telescope.builtin')
+-- set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+-- set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+-- set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+-- set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 return pluginKeys
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -64,52 +64,18 @@ return {
   },
 
   -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-      },
-    },
-  },
-
-  -- add tsserver and setup with typescript.nvim instead of lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
-    },
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- tsserver will be automatically installed with mason and loaded with lspconfig
-        tsserver = {},
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        -- example to setup with typescript.nvim
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   init = function() end,
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@type lspconfig.options
+  --     servers = {
+  --       -- pyright will be automatically installed with mason and loaded with lspconfig
+  --       pyright = {},
+  --     },
+  --   },
+  -- },
 
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
@@ -152,7 +118,7 @@ return {
     end,
   },
 
-  -- the opts function can also be used to change the default opts:
+  -- or you can return new options to override all the defaults
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
@@ -164,18 +130,6 @@ return {
       })
     end,
   },
-
-  -- or you can return new options to override all the defaults
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return {
-        --[[add your custom lualine config here]]
-      }
-    end,
-  },
-
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
