863739ccf9fe6f047f62f4579e1c3a88d262594b
冰咖啡 <z1876849563@163.com>
2025-10-02 10:04:48 +0800

    修改了option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,8 +5,8 @@
 vim.g.encoding = "UTF-8"
 vim.o.fileencoding = "utf-8"
 -- jkhl 移动时光标周围保留8行
-vim.o.scrolloff = 8
-vim.o.sidescrolloff = 8
+vim.o.scrolloff = 20
+vim.o.sidescrolloff = 20
 -- 禁用相对行号
 vim.wo.number = true
 vim.wo.relativenumber = false
@@ -15,7 +15,7 @@ vim.wo.cursorline = true
 -- 显示左侧图标指示列
 vim.wo.signcolumn = "yes"
 -- 右侧参考线，超过表示代码太长了，考虑换行
-vim.wo.colorcolumn = "80"
+-- vim.wo.colorcolumn = "80"
 -- 缩进2个空格等于一个Tab
 vim.o.tabstop = 2
 vim.bo.tabstop = 2
