f91817f232840b547799d0e4afebb9aba31e9e98
冰咖啡 <z1876849563@163.com>
2025-12-10 20:17:37 +0800

    更改了todo配置

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,30 +1,30 @@
 return {
 	"folke/todo-comments.nvim",
-	cmd = { "TodoTrouble", "TodoTelescope" },
 	event = "VeryLazy",
 	opts = {},
-	keys = {
-		{
-			"]t",
-			function()
-				require("todo-comments").jump_next()
-			end,
-			desc = "Next Todo Comment",
-		},
-		{
-			"[t",
-			function()
-				require("todo-comments").jump_prev()
-			end,
-			desc = "Previous Todo Comment",
-		},
-		-- { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
-		-- {
-		-- 	"<leader>xT",
-		-- 	"<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>",
-		-- 	desc = "Todo/Fix/Fixme (Trouble)",
-		-- },
-		{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-		-- { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"folke/snacks.nvim",
 	},
+	keys = {
+		---@diagnostic disable-next-line: undefined-field
+		{
+			"<leader>st",
+			function()
+				require("snacks").picker.todo_comments({
+					keywords = { "TODO", "FIX", "FIXME", "BUG", "FIXIT", "HACK", "WARN", "ISSUE" },
+				})
+			end,
+			desc = "[TODO] Pick todos (without NOTE)",
+		},
+		---@diagnostic disable-next-line: undefined-field
+		{
+			"<leader>sT",
+			function()
+				require("snacks").picker.todo_comments()
+			end,
+			desc = "[TODO] Pick todos (with NOTE)",
+		},
+	},
+	config = true,
 }
