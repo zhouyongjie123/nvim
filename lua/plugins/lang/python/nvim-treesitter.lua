72a0a889805e0b17540bb8e7ccb93a3a8488e0d4
冰咖啡 <z1876849563@163.com>
2026-01-08 11:35:20 +0800

    增加了python中nvim-treesitter的一些配置

--- lua/plugins/lang/python/nvim-treesitter.lua
+++ lua/plugins/lang/python/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	optional = true,
 	opts = {
-		ensure_installed = { "python" },
+		ensure_installed = { "python", "ninja", "rst" },
 	},
 	opts_extend = { "ensure_installed" },
 }
